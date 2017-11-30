3391607ef3dcf3816a653811522bc152883860e0
Madhura Bhave <mbhave@pivotal.io>
2017-11-30 12:59:36 -0800

    Add controller test

---
+++ non-reactive/src/test/java/com/example/car/web/CarsControllerTests.java
@@ -0,0 +1,29 @@
+package com.example.car.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class CarsControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getCar_WithName_ReturnsCar() throws Exception {
+		this.mockMvc.perform(get("/cars/{name}", "prius"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("name").value("prius"))
+				.andExpect(jsonPath("type").value("hybrid"));
+	}
+}
