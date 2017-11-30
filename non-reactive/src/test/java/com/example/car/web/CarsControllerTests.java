386123dda9b6da46fd5fbc09ea2c0d123a7d59e5
Madhura Bhave <mbhave@pivotal.io>
2017-11-30 13:03:12 -0800

    Add controller mapping

--- non-reactive/src/test/java/com/example/car/web/CarsControllerTests.java
+++ non-reactive/src/test/java/com/example/car/web/CarsControllerTests.java
@@ -1,13 +1,17 @@
 package com.example.car.web;
 
+import com.example.car.domain.Car;
+import com.example.car.domain.CarRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,8 +23,12 @@ public class CarsControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	private CarRepository carRepository;
+
 	@Test
 	public void getCar_WithName_ReturnsCar() throws Exception {
+		when(this.carRepository.findByName("prius")).thenReturn(new Car("prius", "hybrid"));
 		this.mockMvc.perform(get("/cars/{name}", "prius"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("name").value("prius"))
