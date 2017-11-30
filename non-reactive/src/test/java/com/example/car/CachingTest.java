5ed58ef16f4f5344c3faa8accce27cde46353c8c
Madhura Bhave <mbhave@pivotal.io>
2017-11-30 13:26:48 -0800

    Use real db

--- non-reactive/src/test/java/com/example/car/CachingTest.java
+++ non-reactive/src/test/java/com/example/car/CachingTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,6 +19,7 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureTestDatabase
 public class CachingTest {
 
 	@Autowired
